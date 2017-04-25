fa96eb6b888e5c384571bbd6967e86ec590e8f00
Yuki Ishikawa <mazeltov17@gmail.com>
2017-04-25 16:13:09 +0900

    edit, destroyも完了

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,19 @@ const models = require('../models');
 
 var router = express.Router();
 
-/* GET users listing. */
+// index
 router.get('/', function(req, res, next) {
   models.User.findAll().then(function(results) {
-  	console.log(`resssssut: ${results}`);
   	res.render('users/index', {users: results});
   });
 });
 
+// new
 router.get('/new', function(req, res, next) {
 	res.render('users/new');
 });
 
+// create
 router.post('/create', function(req, res) {
 	models.User.create({
 		name: req.body.name,
@@ -24,6 +25,7 @@ router.post('/create', function(req, res) {
 	});
 });
 
+// show
 router.get('/:user_id', function(req, res) {
 	models.User
 		.findOne({where: {id: req.params.user_id}})
@@ -31,5 +33,43 @@ router.get('/:user_id', function(req, res) {
 			res.render('users/show', {user: result});
 		});
 });
+
+// edit
+router.get('/:user_id/edit', function(req, res) {
+	models.User
+		.findOne({where: {id: req.params.user_id}})
+		.then(function(result) {
+			res.render('users/edit', {user: result});
+		});
+});
+
+// update
+router.put('/:user_id', function(req, res) {
+	models.User
+		.update({
+			name: req.body.name,
+			age: req.body.age
+		}, {
+			where: {
+				id: req.params.user_id
+			}
+		}).then(function() {
+			res.redirect('/users');
+		});
+});
+
+// destroy
+router.delete('/:user_id', function(req, res) {
+	models.User
+		.destroy({
+			where: {
+				id: req.body.userId
+			}
+		}).then(function() {
+			res.redirect('/users');
+		});
+});
+
+
 
 module.exports = router;
